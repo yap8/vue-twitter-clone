ad0cd66699947e25f3ab0ae9fec0ff0cd626f532
yap8 <nebozhenko.arseny@gmail.com>
2022-05-15 20:30:43 +0300

    made button flex

--- src/components/UI/AppButton.vue
+++ src/components/UI/AppButton.vue
@@ -1,18 +1,18 @@
 <template>
   <button
-    class="w-full text-xl cursor-pointer text-blue-400 border font-semibold py-3 px-4 rounded-full transition hover:bg-slate-100"
+    class="flex items-center justify-center w-full text-xl cursor-pointer text-blue-400 border font-semibold p-3 rounded-full transition hover:bg-slate-100"
     v-if="variant === 'outlined'"
   >
     <slot />
   </button>
   <button
-    class="w-full text-xl cursor-pointer bg-blue-400 text-white font-semibold py-3 px-4 rounded-full transition hover:bg-blue-500"
+    class="flex items-center justify-center w-full text-xl cursor-pointer bg-blue-400 text-white font-semibold p-3 rounded-full transition hover:bg-blue-500"
     v-else-if="variant === 'filled'"
   >
     <slot />
   </button>
   <button
-    class="w-full text-xl cursor-pointer font-semibold py-3 px-4 rounded-full transition hover:bg-slate-100"
+    class="flex items-center justify-center w-full text-xl cursor-pointer font-semibold p-3 rounded-full transition hover:bg-slate-100"
     v-else
   >
     <slot />
