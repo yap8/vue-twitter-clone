88834ef28b5967b1eb9d3a34acd1e932c74c1147
yap8 <nebozhenko.arseny@gmail.com>
2022-05-15 19:39:00 +0300

    added font awesome

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <router-view />
+  <fa icon="coffee" />
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import { createApp } from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 import router from './router';
 import App from './App.vue';
 import store from './store';
 import './index.css';
 
+library.add(fas);
+
 const app = createApp(App);
 
-app.use(store).use(router).mount('#app');
+app.component('fa', FontAwesomeIcon).use(store).use(router).mount('#app');
