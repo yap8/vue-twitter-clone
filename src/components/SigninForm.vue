91fc11e83ba589c8b597a7cc7e45a6caec201c71
yap8 <nebozhenko.arseny@gmail.com>
2022-05-16 00:00:52 +0300

    added submit handler

--- src/components/SigninForm.vue
+++ src/components/SigninForm.vue
@@ -2,7 +2,7 @@
   <h2 class="max-w-md m-auto mb-6 text-3xl font-semibold">
     Create your account
   </h2>
-  <form class="max-w-md m-auto">
+  <form class="max-w-md m-auto" @submit.prevent="handleSubmit">
     <AppInput class="mb-4" type="email" name="email" v-model="email" />
     <AppInput class="mb-4" type="password" name="password" v-model="password" />
     <AppButton class="m-auto" variant="filled">Sign in</AppButton>
@@ -16,6 +16,16 @@ export default {
       email: '',
       password: '',
     };
+  },
+  methods: {
+    handleSubmit() {
+      alert(this.email, this.password);
+
+      // CHECK IF USER EXISTS IN DB AND RETURN TOKEN
+
+      this.email = '';
+      this.password = '';
+    },
   },
 };
 </script>
