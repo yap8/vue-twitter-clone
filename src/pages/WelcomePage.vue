a00924ac64aad479431151a3bd9092520bb94a68
yap8 <nebozhenko.arseny@gmail.com>
2022-05-15 19:59:08 +0300

    added right icon and titles

--- src/pages/WelcomePage.vue
+++ src/pages/WelcomePage.vue
@@ -13,7 +13,14 @@
       />
     </div>
     <!-- right col -->
-    <div class="w-1/2"></div>
+    <div class="flex flex-col w-1/2 p-10">
+      <fa
+        class="text-blue-500 text-5xl mr-auto mb-16"
+        :icon="['fab', 'twitter']"
+      />
+      <h1 class="text-8xl font-bold mb-16">Happening now</h1>
+      <h2 class="text-5xl font-bold">Join Twitter today.</h2>
+    </div>
   </div>
 </template>
 
